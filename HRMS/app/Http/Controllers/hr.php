fe91bb8b9fbefdde333bfec892f1a4e63fbc91b8
Ahmed <aelkholy912@gmail.com>
2023-10-24 22:32:10 +0300

    adding controller view

---
+++ HRMS/app/Http/Controllers/hr.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class hr extends Controller
+{
+    public function presence(){
+        return view('homepage');
+    }
+}
