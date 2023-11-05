cdb5c13e5538877095d1741b5782440787da5d03
Ahmed <aelkholy912@gmail.com>
2023-11-05 19:44:57 +0200

    adding new routes

--- HRMS/routes/web.php
+++ HRMS/routes/web.php
@@ -1,23 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\hr;
-use App\Http\Controllers\UserController;
+// use App\Http\Controllers\hr;
+// use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function(){
-    return view('layouts.admin');
-});
-// Route::get('/', [hr::class, "presence"]);
-Route::post('/register', [UserController::class, "register"]);
+// Route::get('/', function(){
+//     return view('layouts.admin');
+// });
+// // Route::get('/', [hr::class, "presence"]);
+// Route::post('/register', [UserController::class, "register"]);
+
 
